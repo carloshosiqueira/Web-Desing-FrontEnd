13b5554e717a0f8614bb75baacf6eae9d7716f41
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-10-05 16:50:04 -0300

    Aula 05/10 Continuação da prova

---
+++ Provas/VPS UX e UI/assets/script/script.js
@@ -0,0 +1,7 @@
+function mostrarAviso(){
+    alert('Esperava que acontecesse alguma coisa? Na verdade é só eu brincando com o javascript')
+}
+
+function confirmacao(){
+    confirm('Tem certeza que deseja comprar esses items?')
+}
