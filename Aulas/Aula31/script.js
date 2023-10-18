31b646e332dcfefad56ee5ec6525713dbe285990
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-10-18 16:59:41 -0300

    Aula 18/10 - Introdução JavaScript

---
+++ Aulas/Aula31/script.js
@@ -0,0 +1,11 @@
+function function1(){
+    alert('Botão 1 (iniciar)');
+}
+function function2(){
+    alert('Botão 2 (pausar)')
+}
+function inicio(){
+    let nome = window.prompt('Qual o seu nome: ')
+    document.querySelector('#texto')
+    texto.innerHTML = nome
+}
