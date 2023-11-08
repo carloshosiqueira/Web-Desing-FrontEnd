cb08ee90508bf9df21d1c6c88d372609c84d468e
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-11-08 17:17:54 -0300

    Exercicios em JavaScript

---
+++ Aulas/Aula 39/ex10/script.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('resultado')
+
+function acao1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2() {
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function acao3() {
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4() {
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
